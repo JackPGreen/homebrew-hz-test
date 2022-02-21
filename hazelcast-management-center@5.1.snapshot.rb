1dc5ae52bb618fa05a20bff6673c9039162b0d66
devOpsHazelcast <devops@hazelcast.com>
2022-02-21 11:08:14 +0000

    Hazelcast Management Center 5.1-SNAPSHOT release

--- hazelcast-management-center@5.1.snapshot.rb
+++ hazelcast-management-center@5.1.snapshot.rb
@@ -3,7 +3,7 @@ class HazelcastManagementCenterAT51Snapshot < Formula
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.1.snapshot"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
-    sha256 "8439bb652ddb9f83f3353a58ed0cf9056222cb7a770285843f20f2d44c74d8f6"
+    sha256 "c7f5fcecebc31c6b69854d5fbbe2cbc2d6d76a87a2a66ebcd4ec27f5da27b3d2"
 
     depends_on "openjdk" => :recommended
 
@@ -16,9 +16,18 @@ class HazelcastManagementCenterAT51Snapshot < Formula
         end
         (bin/executable_name).write_env_script libexec/"bin/#{executable_name}", Language::Java.overridable_java_home_env
       end
+      etc.install "#{libexec}/config" => "hazelcast"
+      rm_rf libexec/"config"
+      libexec.install_symlink "#{etc}/hazelcast" => "config"
       prefix.install_metafiles
     end
-  
+
+    def caveats
+        <<~EOS
+          Configuration files have been placed in #{etc}/hazelcast.
+        EOS
+    end
+
     def post_install
       exec "echo Hazelcast Management Center has been installed."
     end
