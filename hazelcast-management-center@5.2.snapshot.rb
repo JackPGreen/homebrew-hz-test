baeced49b9279646281c0b3521535bf244523555
devOpsHazelcast <devops@hazelcast.com>
2022-03-03 13:57:13 +0000

    Hazelcast Management Center 5.2-SNAPSHOT release

--- hazelcast-management-center@5.2.snapshot.rb
+++ hazelcast-management-center@5.2.snapshot.rb
@@ -1,4 +1,4 @@
-class HazelcastManagementCenterAT5X < Formula
+class HazelcastManagementCenterAT52Snapshot < Formula
     desc "Tool to run Hazelcast Management Center"
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "5.0.4"
