22103be048a888c0496d3deffd0314fcc593834a
devOpsHazelcast <devops@hazelcast.com>
2025-06-16 16:09:24 +0000

    Hazelcast Management Center 6.0.0-SNAPSHOT release

--- hazelcast-management-center@6.0.0.snapshot.rb
+++ hazelcast-management-center@6.0.0.snapshot.rb
@@ -1,10 +1,9 @@
-class HazelcastManagementCenterSnapshot < Formula
+class HazelcastManagementCenterAT600Snapshot < Formula
     desc "Tool to run Hazelcast Management Center"
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     version "6.0.0-SNAPSHOT"
     url "https://repository.hazelcast.com/download/management-center/hazelcast-management-center-latest-snapshot.tar.gz"
     sha256 "4e36911a48f4ff1c82247a226ef0ff670570d8ff8b92aeb0bdb27533f911c0d6"
-    conflicts_with "hazelcast-management-center@6.0.0.snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.X", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.5.2", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center@5.4.snapshot", because: "you can install only a single hazelcast-management-center package"
@@ -18,6 +17,7 @@ class HazelcastManagementCenterSnapshot < Formula
     conflicts_with "hazelcast-management-center-5.5", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.1", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center-5.0", because: "you can install only a single hazelcast-management-center package"
+    conflicts_with "hazelcast-management-center-snapshot", because: "you can install only a single hazelcast-management-center package"
     conflicts_with "hazelcast-management-center", because: "you can install only a single hazelcast-management-center package"
 
     depends_on "openjdk@21" => :recommended
