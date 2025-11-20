79fcb251b01d56ed81c2a9968374173dde23e20d
JackPGreen <JackPGreen@users.noreply.github.com>
2025-11-20 11:03:14 +0000

    Hazelcast Homebrew Package 5.7.0-SNAPSHOT release

--- hazelcast-enterprise@5.7.0.snapshot.rb
+++ hazelcast-enterprise@5.7.0.snapshot.rb
@@ -2,7 +2,7 @@ class HazelcastEnterpriseAT570Snapshot < Formula
     desc "Hazelcast is a streaming and memory-first application platform for fast, stateful, data-intensive workloads on-premises, at the edge or as a fully managed cloud service."
     homepage "https://github.com/hazelcast/hazelcast-command-line"
     url "https://repository.hazelcast.com/snapshot/com/hazelcast/hazelcast-enterprise-distribution/5.7.0-SNAPSHOT/hazelcast-enterprise-distribution-5.7.0-SNAPSHOT.tar.gz"
-    sha256 "d5d90ec1ca4dc4140db9964d6b9636b4258b854bb3e1377a177820a3f7d5bf3b"
+    sha256 "fecdcb7ddfd2d9bb94ad7c3733393d1056567c4e36b9716816991f82a4f00080"
     conflicts_with "hazelcast-enterprise@6.0.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.6.1.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
     conflicts_with "hazelcast-enterprise@5.6.0.snapshot", because: "you can install only a single hazelcast or hazelcast-enterprise package"
